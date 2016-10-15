bd840a9eb27f10e8cb5a277e5bf298efe07578dd
Khoa Pham <onmyway133@gmail.com>
2016-10-15 18:20:29 +0200

    Update zsh.sh

--- scripts/zsh.sh
+++ scripts/zsh.sh
@@ -6,7 +6,7 @@ setup_oh_my_zsh()
 {
 	show_header "oh-my-zsh https://github.com/robbyrussell/oh-my-zsh"
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-	show_step "Install avit theme"
+	show_step "Install gentoo theme"
 }
 
 setup_zsh_autosuggestions()
